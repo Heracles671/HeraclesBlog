2ed298ab4429cd2d77da3b18d5921a5ee00ccea0
shinny-chenli <li.chen@zozo.cn>
2022-03-25 22:54:52 +0800

    [常用数据结构]更新二叉树介绍

--- Algorithm/BinaryTree.java
+++ Algorithm/BinaryTree.java
@@ -1,13 +1,122 @@
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree {
 
-    class TreeNode {
+    private static class TreeNode {
         int data;
         TreeNode leftNode;
         TreeNode rightNode;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+
+        Integer data = inputList.removeFirst();
+        System.out.println("原始数据：" + data);
+        if (data != null) {
+            node = new TreeNode(data);
+            System.out.println("开始左递归：" + data);
+            node.leftNode = createBinaryTree(inputList);
+            System.out.println("开始右递归：" + data);
+            node.rightNode = createBinaryTree(inputList);
+            System.out.println("递归结束：" + data);
+        }
+        return node;
+    }
+
+    public static void preOrderTraversalWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.leftNode;
+            }
+
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.rightNode;
+            }
+        }
+    }
+
+    public static void preOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.data);
+        preOrderTraversal(node.leftNode);
+        preOrderTraversal(node.rightNode);
+    }
+
+    public static void inOrderTraversalWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                stack.push(treeNode);
+                treeNode = treeNode.leftNode;
+            }
+
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                System.out.println(treeNode.data);
+                treeNode = treeNode.rightNode;
+            }
+        }
+    }
+
+    public static void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderTraversal(node.leftNode);
+        System.out.println(node.data);
+        inOrderTraversal(node.rightNode);
+    }
+
+    public static void postOrderTraversalWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                stack.push(treeNode);
+                stack1.push(treeNode.data);
+                treeNode = treeNode.rightNode;
+            }
+
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.leftNode;
+            }
+        }
+        while (!stack1.isEmpty()) {
+            System.out.println(stack1.pop());
+        }
+    }
+
+    public static void postOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrderTraversal(node.leftNode);
+        postOrderTraversal(node.rightNode);
+        System.out.println(node.data);
     }
 
     /**
@@ -15,7 +124,7 @@ public class BinaryTree {
      * @param root 二叉树根结点
      */
     public static void levelOrderTraversal(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()){
             TreeNode node = queue.poll();
@@ -30,7 +139,16 @@ public class BinaryTree {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] { 4, 4, 6, 5, 3, 2, 8, 1 };
-        System.out.println(Arrays.toString(arr));
+        LinkedList<Integer> inputList = new LinkedList<>(
+                Arrays.asList(3, 2, 9, null, null, 10, null, null, 8, null, 4));
+        TreeNode treeNode = createBinaryTree(inputList);
+        System.out.println("前序遍历：");
+        preOrderTraversal(treeNode);
+        System.out.println("中序遍历：");
+        inOrderTraversal(treeNode);
+        System.out.println("后序遍历：");
+        postOrderTraversal(treeNode);
+        System.out.println("层序遍历：");
+        levelOrderTraversal(treeNode);
     }
 }
